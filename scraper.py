b0979b7ec854dc239e4d52128c2a95b2cbe5d109
mpinchover <mpinchover@gmail.com>
2024-09-08 10:37:29 -0700

    created scraper with reqs

---
+++ scraper.py
@@ -0,0 +1,48 @@
+"""
+# activate env 
+source soundcloud_scraper/bin/activate
+
+# install reqs
+pip install -r requirements.txt
+"""
+import requests
+from bs4 import BeautifulSoup
+import random
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service as ChromeService
+from chromedriver_py import binary_path
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+import time
+import os
+
+
+def play_soundcloud_track(driver):
+    driver.get("https://soundcloud.com/pinch-12/track-20")
+
+    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.TAG_NAME, "title")))
+
+    soup = BeautifulSoup(driver.page_source, "html.parser")
+
+    try:
+        play_button = WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable((By.CLASS_NAME, "sc-button-play"))
+        )
+        play_button.click()
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+
+def run_scraper():
+    options = webdriver.ChromeOptions()
+    # options.add_argument("--headless")  # Run in headless mode
+    svc = webdriver.ChromeService(executable_path=binary_path)
+    driver = webdriver.Chrome(service=svc)
+    play_soundcloud_track(driver)
+    time.sleep(300)
+
+    driver.quit()
+
+if __name__ == "__main__":
+    run_scraper()
